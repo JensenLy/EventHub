c76110081a6971890bf0fd57206a2ab4e2ee7b4b
anthony-vo <s3951749@student.rmit.edu.au>
2025-09-26 06:27:23 +0700

    Migration test for the users table extension

---
+++ src/test/java/au/edu/rmit/sept/webapp/migration/UserProfileMigrationTest.java
@@ -0,0 +1,25 @@
+package au.edu.rmit.sept.webapp.migration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootTest
+public class UserProfileMigrationTest {
+  @Autowired JdbcTemplate jdbc;
+
+  /*Test to see if migration works as expected by having extra columns for the user table */
+  @Test
+  void users_has_profile_columns() {
+    var columns = jdbc.queryForList("""
+      SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
+      WHERE TABLE_NAME='USERS'
+    """);
+    var names = columns.stream().map(m -> ((Map<?,?>)m).get("COLUMN_NAME").toString().toLowerCase()).toList();
+    assertThat(names).contains("display_name","avatar_url","bio","gender","updated_at");
+  }
+}
