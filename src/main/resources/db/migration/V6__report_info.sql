dd61fb8788a6622a4bb98e7f81124ecb57d94fc4
JensenLy <s3977367@student.rmit.edu.au>
2025-10-08 02:14:31 +1100

    created report table

---
+++ src/main/resources/db/migration/V6__report_info.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS reports;
+
+CREATE TABLE reports (
+  report_id IDENTITY PRIMARY KEY,
+  user_id BIGINT NOT NULL,
+  event_id BIGINT NOT NULL,
+  note VARCHAR(1000),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+  CONSTRAINT fk_report_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+  CONSTRAINT fk_report_event FOREIGN KEY (event_id) REFERENCES events(event_id),
+  CONSTRAINT uc_report_event UNIQUE (user_id, event_id)  -- prevent duplicate report
+);
